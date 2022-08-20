1feae4c3b96356c268e06ac9e1dab22722a3e3ca
jorge90125 <jorge.gonzalez7@g.austincc.edu>
2022-08-20 16:11:24 -0500

    User controller and schema set up

--- models/users.js
+++ models/users.js
@@ -1,3 +1,20 @@
 const mongoose = require(`mongoose`)
 
-// module.exports = User
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    email: String,
+    age: Number,
+    location: String
+})
+
+const User = mongoose.model(`User`, userSchema)
+
+module.exports = User
